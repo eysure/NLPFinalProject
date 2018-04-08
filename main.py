1fedddf6f6bc6b862166937d556682ff15cd1669
Henry Zhu <eysure@icloud.com>
2018-04-07 21:23:32 -0500

    Some "UI"

--- main.py
+++ main.py
@@ -7,3 +7,41 @@
 # in whole or in part, without the express written permission of
 # Xinyang Zhu & Alvin Zhang
 
+import xml.etree.ElementTree as ET
+
+print("CS6320 Natural Language Processing Final Project by AXZY")
+
+# read corpus (FAQs)
+faq_tree = None
+faqs = None
+try:
+    faq_tree = ET.parse('FAQs.xml')
+    faqs = faq_tree.getroot()
+except FileNotFoundError:
+    print("😰 Sorry, cannot find FAQ file, "
+          "please put 'FAQs.xml' file at the same dir as this python file and try again. "
+          "Email to xyzhu@utdallas.edu if necessary.")
+    exit(0)
+except ET.ParseError:
+    print("😰 Sorry, we find this 'FAQs.xml' cannot be parsed properly. "
+          "Please check file integrity or re-download the project. "
+          "Email to xyzhu@utdallas.edu if necessary.")
+    exit(0)
+print("✌️ Read FAQs successfully.")
+
+# Listen user input
+while True:
+    u_input = input("Please input your question (Input \"help\" to see manual):\n")
+
+    # Help & Debug
+    if u_input == "help":
+        print("Manual: ")
+    elif u_input == "show raw":
+        ET.dump(faqs)
+    elif u_input == "show":
+        for faq in faqs:
+            print(faq.attrib['id'])
+            print("\tQ: ", faq[0].text.split())
+            print("\tA: ", faq[1].text.split())
+    else:
+        print("Processing your input...")
