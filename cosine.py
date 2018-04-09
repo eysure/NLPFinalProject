80fc3fe5d1ce192306a2a48af1ea2ea87181ebf4
Henry Zhu <eysure@icloud.com>
2018-04-09 03:51:01 -0500

    achieve the task 2, without stop words

---
+++ cosine.py
@@ -0,0 +1,55 @@
+"""
+Final Project - Natural Language Processing - S18
+AXYZ group
+Xinyang Zhu / 2021409044 / XXZ170001 / xyzhu@utdallas.edu / (469)-974-7431
+Alvin Zhang / YXZ165231
+
+This document is the property of Xinyang Zhu & Alvin Zhang
+This document may not be reproduced or transmitted in any form,
+in whole or in part, without the express written permission of
+Xinyang Zhu & Alvin Zhang
+
+cosine.py
+This file is mainly use for Task 2
+"""
+
+import math
+import re
+from collections import Counter
+
+WORD = re.compile(r'\w+')
+
+
+def get_cosine(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x] ** 2 for x in vec1.keys()])
+    sum2 = sum([vec2[x] ** 2 for x in vec2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+
+def text_to_vector(text):
+    words = WORD.findall(text)
+    return Counter(words)
+
+
+def test():
+    print("Input two sentence then I will tell you the cosine distance")
+
+    text1 = input("Sentence 1: ")
+    text2 = input("Sentence 2: ")
+
+    vector1 = text_to_vector(text1)
+    print(vector1)
+    vector2 = text_to_vector(text2)
+    print(vector2)
+
+    cosine = get_cosine(vector1, vector2)
+
+    print('Result:', cosine)
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-# Final Project - Natural Language Processing - S18
-# Xinyang Zhu / 2021409044 / XXZ170001 / xyzhu@utdallas.edu / (469)-974-7431
-# Alvin Zhang / YXZ165231
-#
-# This document is the property of Xinyang Zhu & Alvin Zhang
-# This document may not be reproduced or transmitted in any form,
-# in whole or in part, without the express written permission of
-# Xinyang Zhu & Alvin Zhang
+"""
+Final Project - Natural Language Processing - S18
+AXYZ group
+Xinyang Zhu / 2021409044 / XXZ170001 / xyzhu@utdallas.edu / (469)-974-7431
+Alvin Zhang / YXZ165231
 
-import xml.etree.ElementTree as ET
+This document is the property of Xinyang Zhu & Alvin Zhang
+This document may not be reproduced or transmitted in any form,
+in whole or in part, without the express written permission of
+Xinyang Zhu & Alvin Zhang
+
+main.py
+This is the main entrance of this project, contain main loop and UI
+"""
+
+import re                               # Regular Expression
+import xml.etree.ElementTree as ET      # XML Parser
+from collections import Counter         # List Counter
+from cosine import get_cosine           # For Task 2
+
+TOKENIZER = r"\d+\.\d+|'\w+|[\w\/\-]+"  # Tokenizer
+MAX_SHOW = 10                           # Amount of best result will be shown
 
 print("CS6320 Natural Language Processing Final Project by AXZY")
 
 # read corpus (FAQs)
-faq_tree = None
-faqs = None
+faq_tree, faqs = None, None
 try:
     faq_tree = ET.parse('FAQs.xml')
     faqs = faq_tree.getroot()
@@ -29,11 +40,16 @@ except ET.ParseError:
     exit(0)
 print("✌️ Read FAQs successfully.")
 
+# Establish faq bag
+faq_bag = []
+for faq in faqs:
+    faq_bag.append([re.findall(TOKENIZER, faq[0].text), re.findall(TOKENIZER, faq[1].text)])
+
 # Listen user input
 while True:
     u_input = input("Please input your question (Input \"help\" to see manual):\n")
 
-    # Help & Debug
+    # input & help & debug
     if u_input == "help":
         print("Manual: ")
     elif u_input == "show raw":
@@ -41,7 +57,34 @@ while True:
     elif u_input == "show":
         for faq in faqs:
             print(faq.attrib['id'])
-            print("\tQ: ", faq[0].text.split())
-            print("\tA: ", faq[1].text.split())
+            print("\tQ: ", faq[0].text.strip())
+            print("\tA: ", faq[1].text.strip())
+    elif u_input == "show bag":
+        for faq_entry in faq_bag:
+            print("faq_bag", "["+str(faq_bag.index(faq_entry))+"]")
+            print("\t[0]:", faq_entry[0])
+            print("\t[1]:", faq_entry[1])
+            print()
+    elif u_input == "show bag counter":
+        for faq_entry in faq_bag:
+            print("faq_bag", "["+str(faq_bag.index(faq_entry))+"]")
+            print("\t[0]:", Counter(faq_entry[0]))
+            print("\t[1]:", Counter(faq_entry[1]))
+            print()
     else:
-        print("Processing your input...")
+        input_bag = re.findall(TOKENIZER, u_input.lower())
+
+        # Task 2 - Use statistical method (cosine-similarity) to calculate similarity of  user input and every faqs
+        cos_counter = {}
+        for faq_entry in faq_bag:
+            cos = get_cosine(Counter(input_bag), Counter(faq_entry[0]+faq_entry[1]))
+            cos_counter[faq_bag.index(faq_entry)] = cos
+
+        show_count = 0
+        for index in sorted(cos_counter, key=cos_counter.get, reverse=True):
+            if cos_counter[index] > 0 and show_count < MAX_SHOW:
+                print("FAQ #", index, "\tSimilarity: ", cos_counter[index])
+                print("\tQ:", " ".join(faq_bag[index][0]))
+                print("\tA:", " ".join(faq_bag[index][1]))
+                print()
+                show_count += 1
