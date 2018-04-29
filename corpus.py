55f64b6dda486056c5a389e7d8d76274406203a4
SNCDefenser <snczhangyujie@gmail.com>
2018-04-28 23:59:13 -0500

    finish task4

--- corpus.py
+++ corpus.py
@@ -16,6 +16,7 @@ This file is a module of corpus, which is FAQ data in our project
 from collections import Counter                     # Count the bag of words
 from cosine import get_cosine                       # Get the cos similarity
 import nltk                                         # NLTK - For downloading only
+import pandas as pd                                 # Pandas - create a dataset for train
 from nltk.corpus import stopwords                   # NLTK - Use to identify stop words
 from nltk.tokenize import sent_tokenize             # NLTK - Use to tokenize text to sentences
 from nltk.tokenize import word_tokenize             # NLTK - Use to tokenize text to words
@@ -25,6 +26,7 @@ from nltk.stem.lancaster import LancasterStemmer    # NLTK - A type of Stemmer (
 from nltk.stem import WordNetLemmatizer             # NLTK - WordNet Lemmatizer
 from nltk.corpus import wordnet as wn               # NLTK - WOrdNet interface
 from nltk.parse.stanford import StanfordParser      # Stanford - syntactic tree parser
+import re
 
 # NLTK data downloading
 nltk.download('averaged_perceptron_tagger')
@@ -35,46 +37,94 @@ nltk.download('punkt')
 stop_words = set(stopwords.words('english'))
 stemmer = LancasterStemmer()
 lemmatizer = WordNetLemmatizer()
-parser = StanfordParser('/Users/henry/stanfordNLP/stanford-parser-full-2018-02-27/stanford-parser.jar',
-                        '/Users/henry/stanfordNLP/stanford-english-corenlp-2018-02-27-models.jar')
+parser = StanfordParser('stanford-parser-full-2018-02-27/stanford-parser.jar',
+                        'stanford-english-corenlp-2018-02-27-models.jar')
+TOKENIZER = r"\d+\.\d+|'\w+|[\w\/\-]+"  # Tokenizer
 
+def data_cleaning(string):
+    string = string.lower()
+    return string
 
 # Class of entry of corpus
 class FAQ:
-    def __init__(self, str_q, str_a):
-
+    def __init__(self, str_q, str_a, index):
         # Clean the input (delete unnecessary space)
         self.str_q = str_q.strip()
         self.str_a = str_a.strip()
-
         # Tokenize
-        self.bag_q = word_tokenize(self.str_q)
-        self.bag_a = word_tokenize(self.str_a)
-
-        # POS Tagging (As feature)
-        self.bag_q_pos = nltk.pos_tag(self.bag_q)     # Tuple (word, POS)
-        self.bag_a_pos = nltk.pos_tag(self.bag_a)     # Tuple (word, POS)
+        self.bag_q = re.findall(TOKENIZER, data_cleaning(str_q))
+        self.bag_a = re.findall(TOKENIZER, data_cleaning(str_a))
+        # Tokenize
+        # self.bag_q = word_tokenize(self.str_q)
+        # self.bag_a = word_tokenize(self.str_a)
 
         # Remove stop words
         self.bag_q_sw_removed = [w for w in self.bag_q if w.lower() not in stop_words]
         self.bag_a_sw_removed = [w for w in self.bag_a if w.lower() not in stop_words]
 
+        # POS Tagging (As feature)
+        self.bag_q_pos = nltk.pos_tag(self.bag_q_sw_removed)     # Tuple (word, POS)
+        self.bag_a_pos = nltk.pos_tag(self.bag_a_sw_removed)     # Tuple (word, POS)
+        
+         # Create dataframe to generate the train data
+        self.trainData = pd.DataFrame()
+        trainData_q = pd.DataFrame()
+        trainData_a = pd.DataFrame()
+        
+        tokens_q = []
+        wordTags_q = []
+        values_q = []
+        tokens_a = []
+        wordTags_a = []
+        values_a = []
+        for tag in self.bag_q_pos:
+            tokens_q.append(tag[0])
+            wordTags_q.append(tag[1])
+            values_q.append(index)
+        trainData_q['token'] = tokens_q
+        trainData_q['wordTag'] = wordTags_q
+        trainData_q['value'] = values_q
+        for tag in self.bag_a_pos:
+            tokens_a.append(tag[0])
+            wordTags_a.append(tag[1])
+            values_a.append(index)
+        trainData_a['token'] = tokens_a
+        trainData_a['wordTag'] = wordTags_a
+        trainData_a['value'] = values_a
+        
         # Stem (As feature)
         self.bag_q_stemmed = []
         self.bag_a_stemmed = []
-        for word in self.bag_q:
-            self.bag_q_stemmed.append(stemmer.stem(word))
-        for word in self.bag_a:
-            self.bag_a_stemmed.append(stemmer.stem(word))
-
         # Lemmatize (As feature)
         self.bag_q_lemmatized = []
         self.bag_a_lemmatized = []
-        for word in self.bag_q:
+        stems_q = pd.DataFrame()
+        stems_a = pd.DataFrame()
+        stemWords_q = []
+        stemArray_q = []
+        stemWords_a = []
+        stemArray_a = []
+        lemmatizes_q = []
+        lemmatizes_a = []
+        for word in self.bag_q_sw_removed:
+            self.bag_q_stemmed.append(stemmer.stem(word))
             self.bag_q_lemmatized.append(lemmatizer.lemmatize(word))
-        for word in self.bag_a:
+            stemWords_q.append(word)
+            stemArray_q.append(stemmer.stem(word))
+            lemmatizes_q.append(lemmatizer.lemmatize(word))
+        for word in self.bag_a_sw_removed:
+            self.bag_a_stemmed.append(stemmer.stem(word))
             self.bag_a_lemmatized.append(lemmatizer.lemmatize(word))
-
+            stemWords_a.append(word)
+            stemArray_a.append(stemmer.stem(word))
+            lemmatizes_a.append(lemmatizer.lemmatize(word))
+        stems_q['token'] = stemWords_q
+        stems_q['stem'] = stemArray_q
+        stems_q['lemmas'] = lemmatizes_q
+        stems_a['token'] = stemWords_a
+        stems_a['stem'] = stemArray_a
+        stems_a['lemmas'] = lemmatizes_a
+            
         # Tree Parse (As feature)
         self.sent_q = sent_tokenize(self.str_q)
         self.sent_a = sent_tokenize(self.str_a)
@@ -82,12 +132,25 @@ class FAQ:
         self.parse_tree_a = parser.raw_parse_sents(self.sent_a)
 
         # WordNet hypernymns, hyponyms, meronyms, AND holonyms (As feature)
-        self.hypernymns = []
-        self.hyponyms = []
-        self.meronyms = []
-        self.holonyms = []
+        synsets_q = pd.DataFrame()
+        synsets_a = pd.DataFrame()
+        synsets_q_word = []
+        synsets_a_word = []
+        hypernymns_q = []
+        hyponyms_q = []
+        meronyms_q = []
+        holonyms_q = []
+        hypernymns_a = []
+        hyponyms_a = []
+        meronyms_a = []
+        holonyms_a = []
         self.bag_counter = Counter(self.bag_q_sw_removed) + Counter(self.bag_a_sw_removed)
-        for word in self.bag_counter.keys():
+        for word in self.bag_q_sw_removed:
+            synsets_q_word.append(word)
+            syn_hypernyms = ''
+            syn_hyponyms = ''
+            syn_meronyms = ''
+            syn_holonyms = ''
             synsets = wn.synsets(word)
             if synsets:
                 max_cos = 0.0
@@ -101,15 +164,113 @@ class FAQ:
                 if target_synset is None:
                     target_synset = synsets[0]
                 if target_synset.hypernyms():
-                    self.hypernymns += target_synset.hypernyms()
+                    for hypernym in target_synset.hypernyms():
+                        syn_hypernyms += str(hypernym) + '|'
+                    hypernymns_q.append(syn_hypernyms)
+                else:
+                    hypernymns_q.append('null')
                 if target_synset.hyponyms():
-                    self.hyponyms += target_synset.hyponyms()
+                    for hyponym in target_synset.hyponyms():
+                        syn_hyponyms += str(hyponym) + '|'
+                    hyponyms_q.append(syn_hyponyms)
+                else:
+                    hyponyms_q.append('null')
                 if target_synset.part_meronyms():
-                    self.meronyms += target_synset.part_meronyms()
+                    for meronym in target_synset.part_meronyms():
+                        syn_meronyms += str(meronym) + '|'
+                    meronyms_q.append(syn_meronyms)
+                else:
+                    meronyms_q.append('null')
                 if target_synset.part_holonyms():
-                    self.holonyms += target_synset.part_holonyms()
+                    for holonym in target_synset.part_holonyms():
+                        syn_holonyms += str(holonym) + '|'
+                    holonyms_q.append(syn_holonyms)
+                else:
+                    holonyms_q.append('null')
+            else:
+                hypernymns_q.append('null')
+                hyponyms_q.append('null')
+                meronyms_q.append('null')
+                holonyms_q.append('null')
+        synsets_q['token'] = synsets_q_word
+        synsets_q['hypernyms'] = hypernymns_q
+        synsets_q['hyponyms'] = hyponyms_q
+        synsets_q['meronyms'] = meronyms_q
+        synsets_q['holonyms'] = holonyms_q
+    
+        for word in self.bag_a_sw_removed:
+            synsets_a_word.append(word)
+            syn_hypernyms = ''
+            syn_hyponyms = ''
+            syn_meronyms = ''
+            syn_holonyms = ''
+            synsets = wn.synsets(word)
+            if synsets:
+                max_cos = 0.0
+                target_synset = None
+                for synset in synsets:
+                    definition = synset.definition()
+                    cos = get_cosine(Counter(self.bag_q + self.bag_a), Counter(definition))
+                    if cos > max_cos:
+                        max_cos = cos
+                        target_synset = synset
+                if target_synset is None:
+                    target_synset = synsets[0]
+                if target_synset.hypernyms():
+                    for hypernym in target_synset.hypernyms():
+                        syn_hypernyms += str(hypernym) + '|'
+                    hypernymns_a.append(syn_hypernyms)
+                else:
+                    hypernymns_a.append('null')
+                if target_synset.hyponyms():
+                    for hyponym in target_synset.hyponyms():
+                        syn_hyponyms += str(hyponym) + '|'
+                    hyponyms_a.append(syn_hyponyms)
+                else:
+                    hyponyms_a.append('null')
+                if target_synset.part_meronyms():
+                    for meronym in target_synset.part_meronyms():
+                        syn_meronyms += str(meronym) + '|'
+                    meronyms_a.append(syn_meronyms)
+                else:
+                    meronyms_a.append('null')
+                if target_synset.part_holonyms():
+                    for holonym in target_synset.part_holonyms():
+                        syn_holonyms += str(holonym) + '|'
+                    holonyms_a.append(syn_holonyms)
+                else:
+                    holonyms_a.append('null')
+            else:
+                hypernymns_a.append('null')
+                hyponyms_a.append('null')
+                meronyms_a.append('null')
+                holonyms_a.append('null')
+        synsets_a['token'] = synsets_a_word
+        synsets_a['hypernyms'] = hypernymns_a
+        synsets_a['hyponyms'] = hyponyms_a
+        synsets_a['meronyms'] = meronyms_a
+        synsets_a['holonyms'] = holonyms_a
 
-    def print(self):
+        self.hypernymns = hypernymns_q + hypernymns_a
+        self.hyponyms = hyponyms_q + hyponyms_a
+        self.meronyms = meronyms_q + meronyms_a
+        self.holonyms = holonyms_q + holonyms_a
+        trainData_q = trainData_q.merge(stems_q, on ='token', how ='left')
+        trainData_a = trainData_a.merge(stems_a, on ='token', how ='left')
+        trainData_q = trainData_q.merge(synsets_q, on ='token', how ='left')
+        trainData_a = trainData_a.merge(synsets_a, on ='token', how ='left')
+        self.trainData = trainData_q
+        # self.trainData = trainData_q.append(trainData_a, ignore_index = True)
+        # self.trainData['token'] = self.trainData['token'].astype(str).astype('category')
+        # self.trainData['wordTag'] = self.trainData['wordTag'].astype(str).astype('category')
+        # self.trainData['stem'] = self.trainData['stem'].astype(str).astype('category')
+        # self.trainData['lemmas'] = self.trainData['lemmas'].astype(str).astype('category')
+        # self.trainData['hypernyms'] = self.trainData['hypernyms'].astype(str).astype('category')
+        # self.trainData['hyponyms'] = self.trainData['hyponyms'].astype(str).astype('category')
+        # self.trainData['meronyms'] = self.trainData['meronyms'].astype(str).astype('category')
+        # self.trainData['holonyms'] = self.trainData['holonyms'].astype(str).astype('category')
+
+    def print_all(self):
         print("Q:", self.str_q)
         print("A:", self.str_a)
 
@@ -117,9 +278,9 @@ class FAQ:
         print("Q:", self.bag_q)
         print("A:", self.bag_a)
 
-    def print_counter(self):
-        print("Q:", self.counter_q)
-        print("A:", self.counter_a)
+    # def print_counter(self):
+    #     print("Q:", self.counter_q)
+    #     print("A:", self.counter_a)
 
     def concat(self):
         return self.bag_q+self.bag_a
--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from corpus import FAQ                # Our corpus model class
 from collections import Counter                     # Count the bag of words
 from cosine import get_cosine                       # Get the cos similarity
 import nltk                                         # NLTK - For downloading only
+import numpy as np
 from nltk.corpus import stopwords                   # NLTK - Use to identify stop words
 from nltk.tokenize import sent_tokenize             # NLTK - Use to tokenize text to sentences
 from nltk.tokenize import word_tokenize             # NLTK - Use to tokenize text to words
@@ -27,6 +28,9 @@ from nltk.stem.lancaster import LancasterStemmer    # NLTK - A type of Stemmer (
 from nltk.stem import WordNetLemmatizer             # NLTK - WordNet Lemmatizer
 from nltk.corpus import wordnet as wn               # NLTK - WOrdNet interface
 from nltk.parse.stanford import StanfordParser      # Stanford - syntactic tree parser
+from sklearn.model_selection import train_test_split
+import re
+TOKENIZER = r"\d+\.\d+|'\w+|[\w\/\-]+"  # Tokenizer
 
 # NLTK data downloading
 nltk.download('averaged_perceptron_tagger')
@@ -37,8 +41,8 @@ nltk.download('punkt')
 stop_words = set(stopwords.words('english'))
 stemmer = LancasterStemmer()
 lemmatizer = WordNetLemmatizer()
-parser = StanfordParser('/Users/henry/stanfordNLP/stanford-parser-full-2018-02-27/stanford-parser.jar',
-                        '/Users/henry/stanfordNLP/stanford-english-corenlp-2018-02-27-models.jar')
+parser = StanfordParser('stanford-parser-full-2018-02-27/stanford-parser.jar',
+                        'stanford-english-corenlp-2018-02-27-models.jar')
 
 MAX_SHOW = 10                           # Amount of best result will be shown
 
@@ -49,11 +53,11 @@ faq_tree, faqs = None, None
 try:
     faq_tree = ET.parse('FAQs.xml')
     faqs = faq_tree.getroot()
-except FileNotFoundError:
-    print("😰 Sorry, cannot find FAQ file, "
-          "please put 'FAQs.xml' file at the same dir as this python file and try again. "
-          "Email to xyzhu@utdallas.edu if necessary.")
-    exit(0)
+# except FileNotFoundError:
+#     print("😰 Sorry, cannot find FAQ file, "
+#           "please put 'FAQs.xml' file at the same dir as this python file and try again. "
+#           "Email to xyzhu@utdallas.edu if necessary.")
+#     exit(0)
 except ET.ParseError:
     print("😰 Sorry, we find this 'FAQs.xml' cannot be parsed properly. "
           "Please check file integrity or re-download the project. "
@@ -63,15 +67,66 @@ print("✌️ Parse FAQ file successfully.")
 
 # Establish corpus and extract features
 print("🤔️ Processing FAQ data...")
+import pandas as pd
 corpus = []
+trainData = pd.DataFrame()
+
+def data_cleaning(string):
+    string = string.lower()
+    return string
+
 for faq in faqs:
-    corpus.append(FAQ(faq[0].text, faq[1].text))
+    current = FAQ(faq[0].text, faq[1].text, len(corpus))
+    corpus.append(current)
+    # print current.trainData
+    trainData = trainData.append(current.trainData, ignore_index = True)
     print(len(corpus), "\tProcessed.")
+# faq = faqs[0]
+# current = FAQ(faq[0].text, faq[1].text, len(corpus))
+# trainData = trainData.append(current.trainData, ignore_index = True)
+valueClass = trainData['value']
+trainData = trainData.drop('value', axis=1)
+trainData['token'] = trainData['token'].astype(str).astype('category')
+trainData['wordTag'] = trainData['wordTag'].astype(str).astype('category')
+trainData['stem'] = trainData['stem'].astype(str).astype('category')
+trainData['lemmas'] = trainData['lemmas'].astype(str).astype('category')
+trainData['hypernyms'] = trainData['hypernyms'].astype(str).astype('category')
+trainData['hyponyms'] = trainData['hyponyms'].astype(str).astype('category')
+trainData['meronyms'] = trainData['meronyms'].astype(str).astype('category')
+trainData['holonyms'] = trainData['holonyms'].astype(str).astype('category')
+
+X_tr, X_val, y_tr, y_val = train_test_split(trainData, valueClass, test_size=0.2, random_state=0)
+# a = np.asarray(corpusTraining)
+# # print corpusTraining
+# np.savetxt("test.txt", a, delimiter=",")
+# from sklearn import tree
+# clf = tree.DecisionTreeClassifier()
+# clf.fit(trainData, valueClass)
+
+import lightgbm as lgb
+lgb_train = lgb.Dataset(X_tr, y_tr)
+lgb_val = lgb.Dataset(X_val, y_val)
+params = {
+        'task': 'train',
+        'boosting_type': 'gbdt',
+        'objective': 'regression',
+        'metric': {'l2', 'auc'},
+        'num_leaves': 31,
+        'learning_rate': 0.05,
+        'feature_fraction': 0.9,
+        'bagging_fraction': 0.8,
+        'bagging_freq': 5,
+        'verbose': 0,
+        'min_data': 1
+    }
+
+lgbm_model = lgb.train(params, train_set = lgb_train, valid_sets = lgb_val, verbose_eval=5)
+
 print("✌️ FAQ data processed.")
 
 # Listen user input
 while True:
-    u_input = input("Please input your question (Input \"help\" to see manual):\n")
+    u_input = raw_input("Please input your question (Input \"help\" to see manual):\n")
 
     # input & help & debug
     if u_input == "help":
@@ -81,7 +136,7 @@ while True:
     elif u_input == "show":
         for faq in corpus:
             print(corpus.index(faq))
-            faq.print()
+            # faq.print()
     elif u_input == "show bag":
         for faq in corpus:
             print(corpus.index(faq))
@@ -98,34 +153,56 @@ while True:
 
         # Tokenize
         input_sents = sent_tokenize(input_str)
-        input_bag = word_tokenize(input_str)
+        # input_bag = word_tokenize(input_str)
+        input_bag = re.findall(TOKENIZER, data_cleaning(input_str))
 
         # parse bag to counter
         input_counter = Counter(input_bag)
 
         # POS Tagging (As feature)
+        testData = pd.DataFrame()
         input_pos = nltk.pos_tag(input_bag)  # Tuple (word, POS)
-
+        tokens = []
+        wordTags = []
+        values = []
+        for tag in input_pos:
+            tokens.append(tag[0])
+            wordTags.append(tag[1])
+        testData['token'] = tokens
+        testData['wordTag'] = wordTags
         # Remove stop words
         input_sw_removed = [w for w in input_bag if w.lower() not in stop_words]
 
         # Stem (As feature) & Lemmatize (As feature)
-        input_stemmed = []
-        input_lemmatized = []
-        for word in input_bag:
-            input_stemmed.append(stemmer.stem(word))
-            input_lemmatized.append(lemmatizer.lemmatize(word))
+        stems = pd.DataFrame()
+        stemWords = []
+        stemArray = []
+        lemmatizes = []
+        for word in input_sw_removed:
+            stemWords.append(word)
+            stemArray.append(stemmer.stem(word))
+            lemmatizes.append(lemmatizer.lemmatize(word))
+        stems['token'] = stemWords
+        stems['stem'] = stemArray
+        stems['lemmas'] = lemmatizes
 
         # Tree Parse (As feature)
         input_tree = parser.raw_parse_sents(input_sents)
 
         # WordNet hypernymns, hyponyms, meronyms, AND holonyms (As feature)
+        input_synsets = pd.DataFrame()
+        input_synsets_word = []
         input_hypernymns = []
         input_hyponyms = []
         input_meronyms = []
         input_holonyms = []
         input_bag_counter = Counter(input_sw_removed)
         for word in input_bag_counter.keys():
+            input_synsets_word.append(word)
+            syn_hypernyms = ''
+            syn_hyponyms = ''
+            syn_meronyms = ''
+            syn_holonyms = ''
             synsets = wn.synsets(word)
             if synsets:
                 max_cos = 0.0
@@ -139,14 +216,50 @@ while True:
                 if target_synset is None:
                     target_synset = synsets[0]
                 if target_synset.hypernyms():
-                    input_hypernymns += target_synset.hypernyms()
+                    for hypernym in target_synset.hypernyms():
+                        syn_hypernyms += str(hypernym) + '|'
+                    input_hypernymns.append(syn_hypernyms)
+                else:
+                    input_hypernymns.append('null')
                 if target_synset.hyponyms():
-                    input_hyponyms += target_synset.hyponyms()
+                    for hyponym in target_synset.hyponyms():
+                        syn_hyponyms += str(hyponym) + '|'
+                    input_hyponyms.append(syn_hyponyms)
+                else:
+                    input_hyponyms.append('null')
                 if target_synset.part_meronyms():
-                    input_meronyms += target_synset.part_meronyms()
+                    for meronym in target_synset.part_meronyms():
+                        syn_meronyms += str(meronym) + '|'
+                    input_meronyms.append(syn_meronyms)
+                else:
+                    input_meronyms.append('null')
                 if target_synset.part_holonyms():
-                    input_holonyms += target_synset.part_holonyms()
-
+                    for holonym in target_synset.part_holonyms():
+                        syn_holonyms += str(holonym) + '|'
+                    input_holonyms.append(syn_holonyms)
+                else:
+                    input_holonyms.append('null')
+            else:
+                input_hypernymns.append('null')
+                input_hyponyms.append('null')
+                input_meronyms.append('null')
+                input_holonyms.append('null')
+        input_synsets['token'] = input_synsets_word
+        input_synsets['hypernyms'] = input_hypernymns
+        input_synsets['hyponyms'] = input_hyponyms
+        input_synsets['meronyms'] = input_meronyms
+        input_synsets['holonyms'] = input_holonyms
+        testData = testData.merge(stems, on ='token', how ='left')
+        testData = testData.merge(input_synsets, on ='token', how ='left')
+        
+        testData['token'] = testData['token'].astype(str).astype('category')
+        testData['wordTag'] = testData['wordTag'].astype(str).astype('category')
+        testData['stem'] = testData['stem'].astype(str).astype('category')
+        testData['lemmas'] = testData['lemmas'].astype(str).astype('category')
+        testData['hypernyms'] = testData['hypernyms'].astype(str).astype('category')
+        testData['hyponyms'] = testData['hyponyms'].astype(str).astype('category')
+        testData['meronyms'] = testData['meronyms'].astype(str).astype('category')
+        testData['holonyms'] = testData['holonyms'].astype(str).astype('category')
         # Task 2 - Use statistical method (cosine-similarity) to calculate similarity of  user input and every faqs
         print("\033[7mTask 2 result\033[0m")
         cos_counter = {}
@@ -158,6 +271,21 @@ while True:
         for index in sorted(cos_counter, key=cos_counter.get, reverse=True):
             if cos_counter[index] > 0 and show_count < MAX_SHOW:
                 print("FAQ #", index, "\tSimilarity: ", cos_counter[index])
-                corpus[index].print()
+                corpus[index].print_all()
                 print()
                 show_count += 1
+        print "Result is: "
+        result = lgbm_model.predict(testData)
+        countR = Counter(result)
+        maxValue = 0 
+        maxIndex = 0
+        print countR
+        for index in countR.keys():
+            if countR[index] > maxValue:
+                maxValue = countR[index]
+                maxIndex = index
+        maxIndex = round(maxIndex, 0)
+        result = corpus[int(maxIndex)]
+        print("FAQ #", maxIndex, "\tSimilarity: ", cos_counter[int(maxIndex)])
+        result.print_all()
+    
